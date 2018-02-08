d2984cd8def584fc76aea86218b8afc14e9647e0
Shawn Wilson <shawn@agrian.com>
2018-02-07 21:55:13 -0800

    use default express-generator project skeleton

--- routes/camera.js
+++ routes/camera.js
@@ -2,10 +2,6 @@
 const PythonShell = require('python-shell');
 
 module.exports = function(app) {
-  app.get('/', function(request, response) {
-    request.sendFile('./public/index.html'); // load our public/index.html file
-  });
-
   app.get('/capture-image', function(request, response) {
     PythonShell.run('lib/python/scripts/capture-image.py', function (err) {
       if (err) { throw err };
